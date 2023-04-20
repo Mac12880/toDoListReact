46c4aae4499724cf66774b39b8a00e5115b2f1b5
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-20 20:26:44 +0800

    Update App.jsx

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -55,6 +55,7 @@ function deleteTodo(id) {
 
     <h1 className='header'>Todo List</h1>
     <ul className='list'>
+      {todos.length === 0 && "No Todos"}
       {todos.map(todo => {
         return (
         <li key={todo.id}>
